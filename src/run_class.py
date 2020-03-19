80620fd91f75a623e27b4d2d6e8ea8712edc2012
George Oblapenko <kunstmord@kunstmord.com>
2020-03-19 13:20:21 -0500

    class to create bash files for run automation

--- src/run_class.py
+++ src/run_class.py
@@ -1,10 +1,11 @@
-from typing import Dict, Callable, List
+from typing import Dict, Callable, List, Any
 import copy
 import pandas as pd
 import numpy as np
+import os
 
 
-class Runs():
+class RunsLoader():
     def __init__(self, params: List,
                  load_and_process_data: Callable[[str], pd.DataFrame],
                  n_ensemble=1, verbose=0):
@@ -17,7 +18,6 @@ class Runs():
 
         self._iterate_recursive(self._params, [])
         self._data.reset_index(inplace=True, drop=True)
-        # for ensemble in n_ensemble:
 
     def _iterate_recursive(self, remaining_params: List,
                            params: List):
@@ -56,3 +56,72 @@ class Runs():
                       for x in output_value_names}
 
         return pd.DataFrame(output_arr)
+
+    def save(self, path_to_hdf5: str):
+        pass
+
+
+class RunsCreater():
+    def __init__(self, path_to_bash_file: str,
+                 output_prefix: str,
+                 executable_str: str,
+                 output_files: List[str],  # list of files to be copied
+                 params: List,
+                 sed_param_replacer: Callable[[str], List[str]],
+                 n_ensemble=1, verbose=0):
+        self._path_to_bash_file = path_to_bash_file
+        self._output_prefix = output_prefix
+        self._executable_str = executable_str
+        self._output_files = copy.deepcopy(output_files)
+        self._params = copy.deepcopy(params)
+        self._sed_param_replacer = sed_param_replacer
+        self._n_ensemble = n_ensemble
+        self._n_data = 1
+        self._verbose = verbose
+
+        self._create_bash_file()
+
+
+    def _create_bash_file(self):
+        with open(self._path_to_bash_file, 'w') as f:
+            for param in self._params:
+                param_str = ' '.join([f'"{x}"' for x in param[1]])
+                f.write(f'declare -a {param[0]}_arr=({param_str})\n')
+            f.write('\n')
+
+            for indent, param in enumerate(self._params):
+                indent_str = '\t' * indent
+                f.write(f'{indent_str}for {param[0]} in "${{{param[0]}_arr[@]}}"\n{indent_str}do\n\n')
+                if self._verbose > indent:
+                    indent_str2 = indent_str + '\t'
+                    f.write(f'{indent_str2}echo ${param[0]}\n')
+
+                replacer_str = self._sed_param_replacer(param[0])
+                indent_str2 = indent_str + '\t'
+                f.write(f"{indent_str2}sed -i 's/{replacer_str[0]}/{replacer_str[1]}/g'\n")
+
+            indent_str = '\t' * len(self._params)
+
+            param_dirs = [f'${{{param[0]}}}' for param in self._params]
+            path_to_create = os.path.join(self._output_prefix, *param_dirs)
+
+            if self._n_ensemble > 1:
+                path_to_create = os.path.join(path_to_create, "${ensemblenumber}")
+                f.write(f"{indent_str}for ensemblenumber in {{1..{self._n_ensemble}}}\n{indent_str}do\n")
+                replacer_str = self._sed_param_replacer("ensemblenumber")
+                indent_str += '\t'
+                f.write(f"{indent_str}sed -i 's/{replacer_str[0]}/{replacer_str[1]}/g'\n")
+
+            f.write(f'{indent_str}mkdir -p {path_to_create}\n')
+            f.write(f'{indent_str}{self._executable_str}\n')
+
+            for filename in self._output_files:
+                f.write(f'{indent_str}mv {filename} {path_to_create}\n')
+
+            if self._n_ensemble > 1:
+                indent_str = indent_str[:-1]
+                f.write(f'{indent_str}done\n')
+
+            for indent, param in enumerate(self._params):
+                indent_str = '\t' * (len(self._params) - 1 - indent)
+                f.write(f'{indent_str}done\n')

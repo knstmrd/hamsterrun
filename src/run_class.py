3eaf204e448cafd2e68bd6d30010ab8eb1e7bfe3
George Oblapenko <kunstmord@kunstmord.com>
2020-03-20 16:35:46 -0500

    fixed data loading and got iteration over single parameter working

--- src/run_class.py
+++ src/run_class.py
@@ -21,6 +21,7 @@ class RunsLoader():
         self._param_sizes = [len(x[1]) for x in self._params]
         self._data_columns = []
 
+        self._data_init = False
         self._iterate_recursive(self._params, [])
 
     def _iterate_recursive(self, remaining_params: List,
@@ -39,18 +40,22 @@ class RunsLoader():
             self._data_columns = list(tmp_dfs[0].columns)
             self._dimension_names += self._data_columns
             self._dimension_names += self._param_names
+            self._n_data = len(tmp_dfs[0])
 
-            dimensions = [self._n_ensemble, len(tmp_dfs[0]),
-                          len(tmp_dfs[0].columns)]
-            dimensions += self._param_sizes
-            self._data = np.zeros(dimensions)
+            if not self._data_init:
+                dimensions = [self._n_ensemble, len(tmp_dfs[0]),
+                              len(tmp_dfs[0].columns)]
+                dimensions += self._param_sizes
+                self._data = np.zeros(dimensions)
+                self._data_init = True
 
             array_index = []
             for i, param in enumerate(params):
                 array_index += [self._params[i][1].index(param[1])]
 
             for tmp_df, ensemble in zip(tmp_dfs, range(self._n_ensemble)):
-                this_array_index = tuple([ensemble, ...] + array_index)
+                this_array_index = tuple([ensemble, slice(0, self._n_data),
+                                          slice(0, len(tmp_dfs[0].columns))] + array_index)
                 self._data[this_array_index] = tmp_df[self._data_columns].values.copy()
         else:
             param_and_value = remaining_params[0]
@@ -64,18 +69,34 @@ class RunsLoader():
 
     def iterate_over_parameter(self, parameter: str, fixed_param_values: Dict,
                                output_value_names: List,
-                               ensembling_function=None):
-        if ensembling_function is None:
-            n_ensemble_mult = self._n_ensemble
-        else:
-            n_ensemble_mult = 1
+                               ensembling_functions: List,
+                               data_squish_function):
 
+        array_index = [slice(0, self._n_ensemble), slice(0, self._n_data)]
 
+        array_index += [...]
 
-        output_arr = {x: np.zeros(len(n_ensemble_mult * self._n_data))
-                      for x in output_value_names}
+        param_slices = [-1] * len(self._param_names)
 
-        return pd.DataFrame(output_arr)
+        for i, param_name in enumerate(self._param_names):
+            if param_name == parameter:
+                param_slices[i] = slice(0, self._param_sizes[i])
+            else:
+                param_slices[i] = self._params[i][1].index(fixed_param_values[param_name])
+
+        array_index += param_slices
+
+        array_index = tuple(array_index)
+        output_arr_data = self._data[array_index]
+
+        output = {}
+        for x in output_value_names:
+            for ens_function in ensembling_functions:
+                for i, x in enumerate(output_value_names):
+                    data_squished = data_squish_function(output_arr_data[:,:,self._data_columns.index(x),:])
+                    output[x + '_' + ens_function[0]] = ens_function[1](data_squished[:,:])
+        output[parameter] = self._params[self._param_names.index(parameter)][1]
+        return pd.DataFrame(output)
 
     def save(self, path_to_hdf5: str):
         pass
